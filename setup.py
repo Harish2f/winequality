2b920c57c34580be9895bada007193b8786c32b0
Harish kumar <harish2f@gmail.com>
2021-09-22 10:14:33 +0530

    updated tox & setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="its a wine Quality Prediction package",
+    author="harish2f",
+    packages=find_packages(),
+    license="MIT"
+)
